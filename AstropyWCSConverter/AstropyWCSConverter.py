773a29ef48b5c1730aaf4f5929fa095ac4700ead
RononDex <tinoheuberger@hotmail.com>
2017-10-27 21:32:17 +0200

    Finished work on the converter

--- AstropyWCSConverter/AstropyWCSConverter.py
+++ AstropyWCSConverter/AstropyWCSConverter.py
@@ -1,1 +1,17 @@
-from astropy import wcs
+from astropy.wcs import WCS
+import numpy as np
+import csv
+
+w = WCS('wcs.fits')
+print(w.wcs.name)
+newCsv = "";
+with open('WCS_Transform.csv', 'r') as csvfile:
+    csvReader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in csvReader:
+        px, py = w.wcs_world2pix(float(row[1]), float(row[2]), 1)
+        print("Transformed RA", row[1], " and DEC", row[2], " to X", px, " and Y", py)
+        newCsv = newCsv + str(row[0]) + ";" + str(row[1]) + ";" + str(row[2]) + ";" + str(px) + ";" + str(py) + "\n"
+
+f = open("WCS_Transform_Output.csv","w") 
+f.write(newCsv)
+f.close()
