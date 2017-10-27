b8435153d1be514e5eca1275c3e2094668d25c75
RononDex <tinoheuberger@hotmail.com>
2017-10-27 21:38:00 +0200

    Just some documentation

--- AstropyWCSConverter/AstropyWCSConverter.py
+++ AstropyWCSConverter/AstropyWCSConverter.py
@@ -1,3 +1,13 @@
+#   This script transforms RA/DEC coordinates from an input csv file "WCS_Transform.csv"
+#   to local x/y coordinates on a 2D plane / picture using a wcs.fits file 
+#   The wcs.fits file contains the transformation matrices used to transform the coordinates
+#   The transformation is SIP compatible, meaning it also includes optical distortions from telescope optics
+
+#   Input CSV file should look like this:
+#       [obj_name];[RA];[DEC]
+#   Ouput CSV file will look like this:
+#       [obj_name];[RA];[DEC];[X];[Y]
+
 from astropy.wcs import WCS
 import numpy as np
 import csv
